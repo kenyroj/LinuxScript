6b1126f6503960f7fcadfa0a07b9e0739e02e2bb
kenyroj <kenyroj@gmail.com>
2019-08-21 10:45:07 +0800

    Add GlobalGitConfig and hint the gitconfig setting

--- install.sh
+++ install.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
+# ==== Add Initial.sh in .bashrc ====
 InitName="$PWD/Initial.sh"
-echo "source $InitName" >> ~/.bashrc
+if grep -q $InitName ~/.bashrc ; then
+	echo "Setting of $InitName was in ~/.bashrc"
+else
+	echo "Add Setting of $InitName in ~/.bashrc... "
+	echo "source $InitName" >> ~/.bashrc
+fi
+
+# ==== Sync the gitconfig setting ====
+echo ========
+echo ${COLOR_YLW}"Check the Global gitconfig is linked"${COLOR_NON}", command:"
+echo ${COLOR_GRN}"ln -s $PWD/GlobalGitConfig ~/.gitconfig"${COLOR_NON}
+
+# ==== Add local git config for GitHub,  in .get/config ====
+echo ========
+echo ${COLOR_YLW}"Add local git config for GitHub"${COLOR_NON}", in .get/config"
+echo ${COLOR_GRN}"[user]"
+echo "	name = kenyroj"
+echo "	email = kenyroj@gmail.com"
+echo "	username = kenyroj"${COLOR_NON}
