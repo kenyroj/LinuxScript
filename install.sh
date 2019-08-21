206e7f8e3a120fb5911131e62abe45e929858129
kenyroj <kenyroj@gmail.com>
2019-08-21 10:45:07 +0800

    Add GlobalGitConfig and hint the gitconfig setting

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@
 
 InitName="$PWD/Initial.sh"
 echo "source $InitName" >> ~/.bashrc
+
+# ==== Sync the gitconfig setting ====
+echo ${COLOR_YLW}"Check the Global gitconfig is linked"${COLOR_NON}", command:"
+echo ${COLOR_GRN}"ln -s $PWD/GlobalGitConfig ~/.gitconfig"${COLOR_NON}
