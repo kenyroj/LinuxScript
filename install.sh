1b5b11eeed00e72c8ffb6fea8ee8bfbbdf5fa0e6
kenyroj <kenyroj@gmail.com>
2019-09-03 10:13:49 +0800

    Add tmux config

--- install.sh
+++ install.sh
@@ -9,21 +9,23 @@ else
 	echo "source $InitName" >> ~/.bashrc
 fi
 
-# ==== Sync the gitconfig setting ====
+# ==== Sync the Global gitconfig setting ====
+echo
 echo ========
-echo ${COLOR_YLW}"Check the Global gitconfig is linked"${COLOR_NON}", command:"
-echo ${COLOR_GRN}"ln -s $PWD/GlobalGitConfig ~/.gitconfig"${COLOR_NON}
+echo ${COL_YLW}"Check the Global gitconfig is linked"${COL_NON}", command:"
+echo ${COL_GRN}"ln -s $PWD/GlobalGitConfig ~/.gitconfig"${COL_NON}
 
 # ==== Add local git config for GitHub,  in .get/config ====
+echo
 echo ========
-echo ${COLOR_YLW}"Add local git config for GitHub"${COLOR_NON}", in .get/config"
-echo ${COLOR_GRN}"[user]"
+echo ${COL_YLW}"Add local git config for GitHub"${COL_NON}", in .get/config"
+echo ${COL_GRN}"[user]"
 echo "	name = kenyroj"
 echo "	email = kenyroj@gmail.com"
-echo "	username = kenyroj"${COLOR_NON}
-=======
-echo "source $InitName" >> ~/.bashrc
+echo "	username = kenyroj"${COL_NON}
 
-# ==== Sync the gitconfig setting ====
-echo ${COLOR_YLW}"Check the Global gitconfig is linked"${COLOR_NON}", command:"
-echo ${COLOR_GRN}"ln -s $PWD/GlobalGitConfig ~/.gitconfig"${COLOR_NON}
+# ==== Sync the Tmux setting ====
+echo
+echo ========
+echo ${COL_YLW}"Check the tmux setting is linked"${COL_NON}", command:"
+echo ${COL_GRN}"ln -s $PWD/TmuxConfig ~/.tmux.conf"${COL_NON}
