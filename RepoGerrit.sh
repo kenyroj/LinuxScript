c04f0be7633cfb679df08c86983b9dc4a9bf5688
AKen.Hsu <aken.hsu@mic.com.tw>
2019-10-10 12:25:33 +0800

    Code Optimization for the RepoGerrit script

--- RepoGerrit.sh
+++ RepoGerrit.sh
@@ -3,40 +3,25 @@
 # Param 3: heads or tags
 DoGitPush() {
 	GERRIT_HOST=$1
-	HEAD_OK_FILE=$2
-	PUSH_REF=$3
+	PUSH_REF=$2
 
 	echo -- Push refs/$PUSH_REF/*: $REPO_PROJECT
 	for n in $(git for-each-ref --format='%(refname)' refs/$PUSH_REF);
 	do
-		echo ---- $n
+		echo ---- $REPO_PROJECT: $n
 		git push ssh://aken.hsu@$GERRIT_HOST:29418/$REPO_PROJECT $n;
 	done
 #	git push ssh://aken.hsu@$GERRIT_HOST:29418/$REPO_PROJECT +refs/$PUSH_REF/*
-#	RET=$?
-#	if [ "$RET" = "0" ] ; then
-#		echo == Projrct $REPO_PROJECT pushed successfully.
-#		touch $HEAD_OK_FILE
-#	else
-#		echo == Projrct $REPO_PROJECT pushed FAIL with RET=$RET
-#	fi
 }
 
 EachGitProject() {
 	PUSH_REF=$1
 	GERRIT_HOST=$2
-	THIS_MONTH=`date +"%y%m"`
-	HEAD_OK_FILE=`pwd`/.$THIS_MONTH.repo_$PUSH_REF_OK
 
 	echo
 	echo [`date +"%y%m%d %H%M%S"`] Gerrit Host:$GERRIT_HOST, Current Project: $REPO_PROJECT
-#	exit 0
 
-	if [ -f $HEAD_OK_FILE ] ; then
-		echo -- Project $REPO_PROJECT was up-to-date and do nothing.
-	else
-		DoGitPush $GERRIT_HOST $HEAD_OK_FILE $PUSH_REF
-	fi
+	DoGitPush $GERRIT_HOST $PUSH_REF
 }
 
 EachGitProject heads $*
