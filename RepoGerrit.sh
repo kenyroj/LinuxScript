47845779ac377721da47fc32e42b71a50d5c9cab
AKen.Hsu <aken.hsu@mic.com.tw>
2019-11-04 10:05:12 +0800

    Improve the processing of Gerrit Push each brance script
    Also try to add multi processing method and not finished

--- RepoGerrit.sh
+++ RepoGerrit.sh
@@ -1,28 +1,78 @@
 #!/bin/bash
 
-# Param 3: heads or tags
+# Param 2: heads or tags
 DoGitPush() {
 	GERRIT_HOST=$1
-	PUSH_REF=$2
 
-	echo -- Push refs/$PUSH_REF/*: $REPO_PROJECT
-	for n in $(git for-each-ref --format='%(refname)' refs/$PUSH_REF);
+	echo "[`date +"%y%m%d-%H%M%S"`] Push refs/heads/* of $REPO_PROJECT"
+	for n in $(git for-each-ref --format='%(refname)' refs/heads);
 	do
-		echo ---- $REPO_PROJECT: $n
+		echo [`date +"%y%m%d-%H%M%S"`] -- $REPO_PROJECT: $n
 		git push ssh://aken.hsu@$GERRIT_HOST:29418/$REPO_PROJECT $n;
 	done
-#	git push ssh://aken.hsu@$GERRIT_HOST:29418/$REPO_PROJECT +refs/$PUSH_REF/*
+
+	echo "[`date +"%y%m%d-%H%M%S"`] Push refs/tags/* of $REPO_PROJECT"
+	for n in $(git for-each-ref --format='%(refname)' refs/tags);
+	do
+		echo [`date +"%y%m%d-%H%M%S"`] -- $REPO_PROJECT: $n
+		git push ssh://aken.hsu@$GERRIT_HOST:29418/$REPO_PROJECT $n;
+	done
 }
 
 EachGitProject() {
-	PUSH_REF=$1
-	GERRIT_HOST=$2
+	GERRIT_HOST=$1
 
 	echo
 	echo [`date +"%y%m%d %H%M%S"`] Gerrit Host:$GERRIT_HOST, Current Project: $REPO_PROJECT
 
-	DoGitPush $GERRIT_HOST $PUSH_REF
+	DoGitPush $GERRIT_HOST
 }
 
-EachGitProject heads $*
-EachGitProject tags $*
+EachGitProject $*
+
+# ======== AKen: Try to run each branches of git by multi-processing. TBI. ========
+DoEachLine() {
+	PRJ_NAME=`echo $* | cut -d ' ' -f 1`
+	PRJ_PATH=`echo $* | cut -d ' ' -f 2`
+	GERRIT_HOST=`echo $* | cut -d ' ' -f 3`
+
+	echo "PRJ=$PRJ_NAME : $PRJ_PATH @ $GERRIT_HOST"
+	echo $REPO_HOME
+
+	cd $PRJ_PATH
+	echo [`date +"%Y%m%d-%H%M%S"`] Push refs/heads/* of $PRJ_NAME
+	for n in $(git for-each-ref --format='%(refname)' refs/heads);
+	do
+		echo [`date +"%Y%m%d-%H%M%S"`] -- $PRJ_NAME: $n
+		git push ssh://aken.hsu@$GERRIT_HOST:29418/$PRJ_NAME $n;
+	done
+
+	echo [`date +"%Y%m%d-%H%M%S"`] Push refs/tags/* of $PRJ_NAME
+	for n in $(git for-each-ref --format='%(refname)' refs/tags);
+	do
+		echo [`date +"%Y%m%d-%H%M%S"`] -- $PRJ_NAME: $n
+		git push ssh://aken.hsu@$GERRIT_HOST:29418/$PRJ_NAME $n;
+	done
+	cd $REPO_HOME
+}
+
+MultiProcessGitPush() {
+	if [ -d ".repo" ] ; then
+		export REPO_HOME=`pwd`
+	else
+		echo "Run this script in repo project, check the '.repo' folder"
+		exit 1
+	fi
+
+	GERRIT_HOST=$1
+	TMP_PRJ_FILE="/data/aken.hsu/XD.txt"
+	while read line ; do
+		DoEachLine $line $GERRIT_HOST
+	done  < $TMP_PRJ_FILE
+
+	echo XD Done
+}
+
+
+#GERRIT_HOST=$1
+#MultiProcessGitPush $GERRIT_HOST
