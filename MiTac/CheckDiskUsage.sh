e2d6eb5239fcac887d1bdb81853f8dc89e5d859e
kenyroj <kenyroj@gmail.com>
2019-11-27 03:33:50 +0800

    Add checking Disk available space Info.

--- MiTac/CheckDiskUsage.sh
+++ MiTac/CheckDiskUsage.sh
@@ -1,7 +1,7 @@
 SAFE_FILE=/data/apbcrdd5/.DiskUsage
 
 echo Disk usage last check date: `date +"%Y/%m/%d %H-%M-%S"`. > $SAFE_FILE
-
+df -H  | grep -e Filesystem -e sdb1 >> $SAFE_FILE
+echo ============================================================= >> $SAFE_FILE
 cd /data
 du -h --max-depth=1 -BM | sort -n >> $SAFE_FILE
-
