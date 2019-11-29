61dab75195dc1c86cdd29088b0b631ad8c0497aa
kenyroj <kenyroj@gmail.com>
2019-11-29 13:27:37 +0800

    Update sc66-android-evb build command

--- Jenkins/sc66-android-evb-dev.sh
+++ Jenkins/sc66-android-evb-dev.sh
@@ -4,16 +4,29 @@ echo BUILD_NUMBER=$BUILD_NUMBER
 echo JENKINS_URL=$JENKINS_URL
 echo BUILD_URL=$BUILD_URL
 
+echo ======== Variables From Parameters ========
+echo DailyManifest=$DailyManifest
+echo BranchName=$BranchName
+
 echo ======== ENV Setting for Jenkins ========
 export PATH=$PATH:.
+GERRIT_URL=gerrit://main.apbcrdd5.mdt
+PRJ_NAME=$BranchName
+BUILD_TYPE="daily"
 
-PRJ_NAME=sc66-android-evb-dev
-BUILD_TYPE=daily
-BUILD_TIME=`date +"%y%m%d-%H%M%S"`
+if [ ! -z $DailyManifest ] ; then
+	BUILD_TIME=$DailyManifest
+else
+	BUILD_TIME=`date +"%Y%m%d-%H%M%S"`
+fi
+
 ANDROID_FOLDER=Android
 NHLOS_FOLDER=NonHLOS
-SRC_PATH=/data/src/${PRJ_NAME}-${BUILD_TIME}
-DPL_PATH=/mnt/nfs/Jenkins/$PRJ_NAME/$BUILD_TYPE
+SRC_HOME=/data/src
+DPL_HOME=/mnt/nfs/Jenkins
+BUILD_NAME=${PRJ_NAME}-${BUILD_TYPE}-${BUILD_TIME}
+SRC_PATH=$SRC_HOME/$BUILD_NAME
+DPL_PATH=$DPL_HOME/$PRJ_NAME/$BUILD_TYPE
 
 mkdir -p $SRC_PATH
 cd $SRC_PATH
@@ -21,8 +34,11 @@ mkdir $ANDROID_FOLDER
 cd $ANDROID_FOLDER
 
 echo ======== Fetch Code ========
-repo init -u gerrit://main.apbcrdd5.mdt/manifest.git -b sc66-android-evb-dev --reference /mnt/nfs/CodeAuroraMirror/
-repo sync
+if [ ! -z $DailyManifest ] ; then
+	REPO_MANIFEST="-m daily/$DailyManifest.xml"
+fi
+repo init -u $GERRIT_URL/manifest.git -b $PRJ_NAME $REPO_MANIFEST --reference /mnt/nfs/CodeAuroraMirror/
+repo sync -c -j4
 
 echo ======== Start Building ========
 source build/envsetup.sh
@@ -31,14 +47,14 @@ make -j4
 
 echo ======== Generate flat_bin Image ========
 cd ..
-git clone -b sc66-android-evb-dev gerrit://main.apbcrdd5.mdt/sc66-nhlos-prebuilt $NHLOS_FOLDER
+git clone -b $PRJ_NAME $GERRIT_URL/sc66-nhlos-prebuilt $NHLOS_FOLDER
 cd $NHLOS_FOLDER
 bash update_common_build.sh ../$ANDROID_FOLDER
 
 echo ======== Deploy Built Image ========
 cd flat_bin
 mkdir -p $DPL_PATH
-zip -r $DPL_PATH/${PRJ_NAME}-${BUILD_TYPE}-${BUILD_TIME}.zip *
+zip -r $DPL_PATH/${BUILD_NAME}.zip *
 
-cd ../..
+cd $SRC_HOME
 rm -rf $SRC_PATH
