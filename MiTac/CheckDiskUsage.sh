f3be8542bbdd0c55c954ad02a4bd7efc1ea3a983
kenyroj <kenyroj@gmail.com>
2020-11-27 16:56:14 +0800

    Update CheckDiskUsage for show /data only

--- MiTac/CheckDiskUsage.sh
+++ MiTac/CheckDiskUsage.sh
@@ -2,7 +2,7 @@ SAFE_FILE=/home/.DiskUsage
 DST_PATH=/data
 
 echo Disk usage last check date: `date +"%Y/%m/%d %H-%M-%S"`. > $SAFE_FILE
-df -H  | grep -e Filesystem -e $DST_PATH >> $SAFE_FILE
+df -H  | grep -e Filesystem -e $DST_PATH\$ >> $SAFE_FILE
 echo ============================================================= >> $SAFE_FILE
 cd $DST_PATH
 du -h --max-depth=1 -BM | sort -n >> $SAFE_FILE
