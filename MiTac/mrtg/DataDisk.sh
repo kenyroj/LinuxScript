ae91979259bc1e9853d9ec348ea9067f8ed07316
kenyroj <kenyroj@gmail.com>
2020-12-17 20:50:03 +0800

    Update datadisk check scrpt

--- MiTac/mrtg/DataDisk.sh
+++ MiTac/mrtg/DataDisk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Shows UsedSpace(in Byte)
 
-echo $((`df -k | grep dev/sdb1 | sed 's/  */ /g' | cut -d ' ' -f 3` * 1))
-echo $((`df -k | grep dev/sdb1 | sed 's/  */ /g' | cut -d ' ' -f 2` * 1))
+echo $((`df -k | grep data$ | sed 's/  */ /g' | cut -d ' ' -f 3` * 1))
+echo $((`df -k | grep data$ | sed 's/  */ /g' | cut -d ' ' -f 2` * 1))
