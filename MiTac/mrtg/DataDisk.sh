4ff18b0ec6e2da0acdf3d642f1d9e668b9d503c0
kenyroj <kenyroj@gmail.com>
2021-02-23 20:46:00 +0800

    Modify the usage available

--- MiTac/mrtg/DataDisk.sh
+++ MiTac/mrtg/DataDisk.sh
@@ -3,5 +3,5 @@
 
 Used=$((`df -k | grep data$ | sed 's/  */ /g' | cut -d ' ' -f 3` * 1))
 Total=$((`df -k | grep data$ | sed 's/  */ /g' | cut -d ' ' -f 2` * 1))
-echo $Used
 echo $((Total-Used))
+echo 0
