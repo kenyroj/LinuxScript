5549dc9de20d7a78526d00925c386ad1b7fea7eb
kenyroj <kenyroj@gmail.com>
2023-09-01 17:23:22 +0800

    Modify the script of MRTG

--- MiTac/mrtg/DataDisk.sh
+++ MiTac/mrtg/DataDisk.sh
@@ -3,6 +3,7 @@
 
 #Used=$((`df -k | grep data$ | sed 's/  */ /g' | cut -d ' ' -f 3` * 1))
 #Total=$((`df -k | grep data$ | sed 's/  */ /g' | cut -d ' ' -f 2` * 1))
-Avail=$((`df -k | grep data$ | sed 's/  */ /g' | cut -d ' ' -f 4`))
-echo $Avail
-echo 0
+AvailData=$((`df -k | grep data$ | sed 's/  */ /g' | cut -d ' ' -f 4`))
+AvailRoot=$((`df -k | grep /$ | sed 's/  */ /g' | cut -d ' ' -f 4`))
+echo $AvailData
+echo $AvailRoot
--- MiTac/mrtg/MemPhy.sh
+++ MiTac/mrtg/MemPhy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Shows UsedSpace(in Byte)
 
-echo $((`free | grep Mem | sed 's/  */ /g' | cut -d ' ' -f 4` * 1))
-echo $((`free | grep Mem | sed 's/  */ /g' | cut -d ' ' -f 3` * 1))
+echo $((`free | grep Mem | sed 's/  */ /g' | cut -d ' ' -f 7` * 1))
+echo $((`free | grep Mem | sed 's/  */ /g' | cut -d ' ' -f 2` * 1))
