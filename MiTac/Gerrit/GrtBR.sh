e5e4f23ad6d19c327115af54d9554be9100c2776
kenyroj <kenyroj@gmail.com>
2024-06-28 17:20:44 +0800

    Add gerrit backup restore script

---
+++ MiTac/Gerrit/GrtBR.sh
@@ -0,0 +1,35 @@
+Usage() {
+cat << EOF
+Usage: Param1:
+ - B: for backup
+ - R: for restore
+EOF
+}
+
+RSync() {
+	rsync -av --delete $1/* $2 
+}
+
+Backup() {
+	RSync /data/Repositories    /mnt/smb/CmBackUp/Repos/
+	RSync /home/gerrit/MainSite	/mnt/smb/CmBackUp/Site/
+}
+
+Restore() {
+	RSync /mnt/smb/CmBackUp/Repos/ /data/GrtRepo
+	RSync /mnt/smb/CmBackUp/Site/  /data/GrtSite
+}
+
+if [ "$1" == "" ] ; then 
+	echo Need 1 Param
+	Usage
+#	return 1
+elif [ "$1" == "B" -o "$1" == "b" ] ; then 
+	Backup
+elif [ "$1" == "R" -o "$1" == "r" ] ; then 
+	echo RRRR
+else
+	echo Wrong param
+	Usage
+#	return 1
+fi
