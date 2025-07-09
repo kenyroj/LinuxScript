98430940cd609ec310d8bdab1dd1921767bce2ae
kenyroj <kenyroj@gmail.com>
2025-07-09 17:22:45 +0800

    Change the way to get the info from df - data disk

--- MiTac/mrtg/DataDisk.sh
+++ MiTac/mrtg/DataDisk.sh
@@ -3,7 +3,7 @@
 
 #Used=$((`df -k | grep data$ | sed 's/  */ /g' | cut -d ' ' -f 3` * 1))
 #Total=$((`df -k | grep data$ | sed 's/  */ /g' | cut -d ' ' -f 2` * 1))
-AvailData=$((`df -l -k | grep data$ | sed 's/  */ /g' | cut -d ' ' -f 4`))
+AvailData=$((`df -l -k | grep vg1-lv1 | sed 's/  */ /g' | cut -d ' ' -f 4`))
 AvailRoot=$((`df -l -k | grep /$ | sed 's/  */ /g' | cut -d ' ' -f 4`))
 echo $AvailData
 echo $AvailRoot
